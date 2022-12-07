c087e1a85f5378aaa633108a104096e2149c0e49
Berkin Kağan Ateş <99278101+berkinKagan@users.noreply.github.com>
2022-12-07 20:26:08 +0300

    Firebase added to the app

--- app/src/main/java/com/burakb/firstproject/MainActivity.java
+++ app/src/main/java/com/burakb/firstproject/MainActivity.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.login);
 
 
     }
