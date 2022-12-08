367a2a2d2ac4ce172e4a05b00899ea1229ff9c10
Berkin Kağan Ateş <kagan.ates@ug.bilkent.edu.tr>
2022-12-08 17:41:57 +0300

    Firebase added to the app fully correct (I hope)

--- app/src/main/java/com/burakb/firstproject/MainActivity.java
+++ app/src/main/java/com/burakb/firstproject/MainActivity.java
@@ -1,12 +1,13 @@
 package com.burakb.firstproject;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
