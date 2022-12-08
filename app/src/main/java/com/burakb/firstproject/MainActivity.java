805691346e93da24d5fcca45b9b0b4ef462f1712
Burak Baştuğ <99401604+BurakBastug@users.noreply.github.com>
2022-12-08 22:13:10 +0300

    some edit and a profile pages were added

--- app/src/main/java/com/burakb/firstproject/MainActivity.java
+++ app/src/main/java/com/burakb/firstproject/MainActivity.java
@@ -1,31 +1,23 @@
 package com.burakb.firstproject;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
+//import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.login);
 
 
 
-    }
-
-    public void writeNewUser(String username, String password, String email){
-        User user = new User(username,password,email);
-        mDatabase.child("users").child(username).setValue(user);
     }
 
 
