4aa813c7536bfaf466413d63739008c12e062a3f
Arda <ardaoztrk@yahoo.com>
2022-12-09 20:13:13 +0300

    Added some login functionalities and some small changes in Register and Login layouts.

--- app/src/main/java/com/burakb/firstproject/LoginActivity.java
+++ app/src/main/java/com/burakb/firstproject/LoginActivity.java
@@ -1,18 +1,73 @@
 package com.burakb.firstproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText txtEMail, txtPassword;
+    private Button loginButton, toRegisterButton;
+    FirebaseAuth mAuth;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        txtEMail = findViewById(R.id.email);
+        txtPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginbtn);
+        toRegisterButton = findViewById(R.id.registerButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = txtEMail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mail)) {
+                    Toast.makeText(LoginActivity.this, "Mail is required", Toast.LENGTH_LONG).show();
+                }
+                if(TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Password is required", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            }
+                            else {
+                                txtPassword.setError("User not found");
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        toRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/burakb/firstproject/RegisterActivity.java
+++ app/src/main/java/com/burakb/firstproject/RegisterActivity.java
@@ -16,8 +16,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -54,33 +52,27 @@ public class RegisterActivity extends AppCompatActivity {
         String psw = password.getText().toString();
         String pswAgain = passwordCheck.getText().toString();
 
-
         if(TextUtils.isEmpty(name)) {
-            Toast.makeText(RegisterActivity.this, "Don't forget entering your name", Toast.LENGTH_LONG).show();
+            Toast.makeText(RegisterActivity.this, "Don't forget to enter your name", Toast.LENGTH_LONG).show();
             userName.setError("Full name is required");
-            //userName.requestFocus();
         }
         if(TextUtils.isEmpty(psw)) {
-            Toast.makeText(RegisterActivity.this, "Don't forget entering your password", Toast.LENGTH_LONG).show();
+            Toast.makeText(RegisterActivity.this, "Don't forget to enter your password", Toast.LENGTH_LONG).show();
             password.setError("Password is required");
-            //password.requestFocus();
         }
         if(TextUtils.isEmpty(pswAgain)) {
-            Toast.makeText(RegisterActivity.this, "Don't forget entering your password again", Toast.LENGTH_LONG).show();
+            Toast.makeText(RegisterActivity.this, "Don't forget to enter your password again", Toast.LENGTH_LONG).show();
             passwordCheck.setError("Full name is required");
-            //passwordCheck.requestFocus();
         }
         if(TextUtils.isEmpty(mail)) {
-            Toast.makeText(RegisterActivity.this, "Don't forget entering your email", Toast.LENGTH_LONG).show();
+            Toast.makeText(RegisterActivity.this, "Don't forget to enter your email", Toast.LENGTH_LONG).show();
             email.setError("E-mail is required");
-            //email.requestFocus();
         }
         if(!password.getText().toString().equals(passwordCheck.getText().toString()) || TextUtils.isEmpty(psw) ) {
             Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_LONG).show();
             passwordCheck.setError("Different password");
-            //passwordCheck.requestFocus();
         }
-        else{
+        else {
             mAuth.createUserWithEmailAndPassword(mail, psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
@@ -91,7 +83,6 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             });
         }
-
     }
 
     private void sendUserToNextActivity() {
