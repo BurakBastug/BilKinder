0b07d34aa35704af669bf128458b0818b206ab3d
Arda <ardaoztrk@yahoo.com>
2022-12-10 21:58:39 +0300

    Added separation to student and teacher login

--- app/src/main/java/com/burakb/firstproject/LoginActivity.java
+++ app/src/main/java/com/burakb/firstproject/LoginActivity.java
@@ -15,12 +15,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText txtEMail, txtPassword;
     private Button loginButton, toRegisterButton;
     FirebaseAuth mAuth;
+    DatabaseReference mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +39,10 @@ public class LoginActivity extends AppCompatActivity {
         toRegisterButton = findViewById(R.id.registerButton);
 
         mAuth = FirebaseAuth.getInstance();
+        mData = FirebaseDatabase.getInstance("https://bilkinderdata-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Users");
 
         loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 String mail = txtEMail.getText().toString().trim();
@@ -47,11 +55,29 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Password is required", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    //if the user is teacher or not
                     mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()) {
+                                mData.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if(snapshot.child("Users").child("Students").exists()) {
+                                            Toast.makeText(LoginActivity.this, "Student login", Toast.LENGTH_LONG).show();
+                                            // TODO: 10.12.2022 go to student home page if profile data is not set up, else go to starteditprofile page
+                                        }
+                                        else {
+                                            Toast.makeText(LoginActivity.this, "Teacher login", Toast.LENGTH_LONG).show();
+                                            // TODO: 10.12.2022 go to teacher home page
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
                                 Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
                                 startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                             }
